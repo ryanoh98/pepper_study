1065dbf88508682d87cbd261aea51cecf6dfe78e
ryanoh98 <sejinryanoh@gmail.com>
2020-07-22 13:54:30 +0900

    forth commit

--- pepper.py
+++ pepper.py
@@ -4,134 +4,36 @@ import sys
 
 # post to net : add -> commit -> push
 # update to net : pull
-## add comment d
-def main():
-    session = qi.Session()
-    ip = '192.168.1.45'
-    port = '9559'
-    try:
-        session.connect("tcp://" + ip + ":" + str(port))
-    except RuntimeError:
-        print ("Can't connect to Naoqi at ip \"" + ip + "\" on port " + str(port) +".\n"
-               "Please check your script arguments. Run with -h option for help.")
-        sys.exit(1)
-
-    # Get the services ALMotion and ALRobotPosture
-    motion_service = session.service("ALMotion")
-    posture_service = session.service("ALRobotPosture")
-
-    # Wake up robot
-    motion_service.wakeUp()
-
-    # Send robot to Pose Init
-    posture_service.goToPosture("StandInit", 0.5)
-
-    # Call Pepper
+import qi
+import argparse
+import sys
 
 
-    # Say "What can I help you, sir?"
+def main(session):
+    """
+    This example uses the say method.
+    It makes the robot say some text using the module.
+    """
+    # Get the service ALTextToSpeech.
+    tts = session.service("ALTextToSpeech")
 
+    #Says a test std::string
+    tts.say("Hi!")
 
-    # Coffee!
-
-
-    # Say "Yes, sir."
-
-
-    # Send robot to the refrigerator
-    # Use several "moveTo" commands
-    # Example of the command
-    x  = 0.2
-    y  = 0.2
-    theta  = math.pi/2
-    motion_service.moveTo(x, y, theta)
-
-    # Open the right hand
-    motion_service.openHand('RHand')
-
-    # Raise the right arm
-
-
-    # Move robot to the left a bit to grab
-    x  = 0.2
-    y  = 0.2
-    theta  = math.pi/2
-    motion_service.moveTo(x, y, theta)
-
-
-    # Close the right hand
-    motion_service.closeHand('RHand')
-
-    # Move back to open the refrigerator
-    x  = 0.2
-    y  = 0.2
-    theta  = math.pi/2
-    motion_service.moveTo(x, y, theta)
-
-    # Release the refrigerator (Open the hand and move back a bit)
-    motion_service.openHand('RHand')
-    x  = 0.2
-    y  = 0.2
-    theta  = math.pi/2
-    motion_service.moveTo(x, y, theta)
-
-    # Send robot to Pose Init
-    posture_service.goToPosture("StandInit", 0.5)
-
-
-    # Place robot in front of the refrigerator (not close enough)
-    x  = 0.2
-    y  = 0.2
-    theta  = math.pi/2
-    motion_service.moveTo(x, y, theta)
-
-
-    # Raise the right arm
-
-
-    # Open the right hand
-    motion_service.openHand('RHand')
-
-    # Move forward a bit
-    x  = 0.2
-    y  = 0.2
-    theta  = math.pi/2
-    motion_service.moveTo(x, y, theta)
-
-
-    # Close the right hand to grab
-    motion_service.closeHand('RHand')
-
-    # Move backward
-    x  = 0.2
-    y  = 0.2
-    theta  = math.pi/2
-    motion_service.moveTo(x, y, theta)
-
-
-    # Arm with 45 degrees
-
-
-    # Come back to the origin
-
-
-    # Drop the coffee
-
-
-    # Say "Here you are, sir. Enjoy!"
-
-
-    # Move backward a bit
-
-
-    # Bow
-
-
-    # Send robot to Pose Init
-    posture_service.goToPosture("StandInit", 0.5)
-
-    # Go to rest position
-    motion_service.rest()
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", type=str, default="192.168.1.45",
+                        help="Robot IP address. On robot or Local Naoqi: use '192.168.1.45'.")
+    parser.add_argument("--port", type=int, default=9559,
+                        help="Naoqi port number")
+
+    args = parser.parse_args()
+    session = qi.Session()
+    try:
+        session.connect("tcp://" + args.ip + ":" + str(args.port))
+    except RuntimeError:
+        print ("Can't connect to Naoqi at ip \"" + args.ip + "\" on port " + str(args.port) +".\n"
+               "Please check your script arguments. Run with -h option for help.")
+        sys.exit(1)
+    main(session)
