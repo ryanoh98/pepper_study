c36ae5f6b7837085519baded7a056b17938d72a9
ryanoh98 <sejinryanoh@gmail.com>
2020-07-24 14:51:30 +0900

    first pose commit

---
+++ firstpose.py
@@ -0,0 +1,22 @@
+import json
+import base64
+import requests
+from PIL import Image
+
+def pose_detect(file):
+    APP_KEY = '3ac17bc0e257b604d053901085eaae99'
+    session = requests.Session()
+    session.headers.update({'Authorization': 'KakaoAK ' + APP_KEY})
+
+    response = session.post('https://cv-api.kakaobrain.com/pose', files=[('file', file)])
+    result = response.json()
+    return result
+
+if __name__ == "__main__":
+    im = Image.open('C:/Users/오윤제/Pictures/1.jpg')
+    print('image_size', im.size)
+
+    f = open('C:/Users/오윤제/Pictures/1.jpg', 'rb')
+    print(f)
+    result_pose = pose_detect(f)
+    print(result_pose)
